b9d649a995bd56059dde8f750885551716ab14d4
lauren-m-johnson <laurenmj90@gmail.com>
2023-08-08 11:47:57 -0700

    initial commit

---
+++ src/NewSkillForm.jsx
@@ -0,0 +1,17 @@
+export default function NewSkillForm() {
+    return (
+      <form action="">
+        <label htmlFor="">Skill</label>
+        <input></input>
+        <label>Level</label>
+        <select>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+        <button>ADD SKILL</button>
+      </form>
+    );
+  }
---
+++ src/SkillList.jsx
@@ -0,0 +1,13 @@
+import SkillListItem from "./SkillListItem";
+
+export default function SkillList() {
+  return (
+    <>
+        <ul>
+        <SkillListItem />
+        <SkillListItem />
+        <SkillListItem />
+        </ul>
+    </>
+  );
+}
